c7a34c2edf183fe7c6d3bfe04b7513e4070efb88
Andrew H. Yi <andrew.yi50@gmail.com>
2013-04-30 17:28:49 -0400

    All pages now display a static google map.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-  function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(-34.397, 150.644),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
-});
+// $(document).ready(function() {
+//   function initialize() {
+//     var mapOptions = {
+//       center: new google.maps.LatLng(-34.397, 150.644),
+//       zoom: 8,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//     var map = new google.maps.Map(document.getElementById("map-canvas"),
+//         mapOptions);
+//   }
+//   google.maps.event.addDomListener(window, 'load', initialize);
+// });
 
 
 // var map;
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,5 @@
 class Destination < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :todo_items
 end
