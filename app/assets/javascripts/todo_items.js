09387a190a41445a13a8b15362b5c1c762c13d20
Andrew H. Yi <andrew.yi50@gmail.com>
2013-04-30 00:06:34 -0400

    Fix AJAX so that new Todo items only get added once to the DOM.

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $('body').bind("ajax:success", function(e, data) {
-    $('.comments').append(data);
     var itemId = $('<div>').append(data).children().first().data("item-id");
     $('.comments-' + itemId).append(data);
   });
