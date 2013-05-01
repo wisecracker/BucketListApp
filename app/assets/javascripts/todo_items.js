15f47288155bd04b75c897c6a89fd64b81fc2914
Andrew H. Yi <andrew.yi50@gmail.com>
2013-04-30 23:32:52 -0400

    Add button that displays/hides all comments on index page.

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,3 +1,5 @@
+// Below function is used at: todo_items/new
+
 $(document).ready(function () {
   $('body').bind("ajax:success", function(e, data) {
     // x = data
@@ -6,4 +8,27 @@ $(document).ready(function () {
     // console.log(itemId);
     $('#comments').append(data);
   });
+
+// $(document.body).click(function () {
+// if ($("div:first").is(":hidden")) {
+// $("div").slideDown("slow");
+// } else {
+// $("div").hide();
+// }
+// });
+  $("div#button").click(function() {
+    if ($("div.todo-item").is(":hidden")) {
+      $("div.todo-item").slideDown("slow");
+    } else {
+      $("div.todo-item").hide();
+    }
+  });
 });
+
+
+
+
+
+
+
+
