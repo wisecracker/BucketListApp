8287e5b2d0a3599a495676883e5f020ab82a17d0
Andrew H. Yi <andrew.yi50@gmail.com>
2013-05-01 03:08:32 -0400

    Add google maps JS function to get new map.

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -30,3 +30,64 @@
 //     title: title
 //   });
 // }
+
+
+// function initialize() {
+    //   var mapOptions = {
+    //     center: new google.maps.LatLng(-34.397, 150.644),
+    //     zoom: 8,
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP
+    //   };
+    //   var map = new google.maps.Map(document.getElementById("#map-canvas"[0]),
+    //       mapOptions);
+    // }
+    // google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+  // var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+  var myLatlng = new google.maps.LatLng(40.7484, -73.9847); //Empire State Building
+  var mapOptions = {
+    zoom: 13,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  var marker = new google.maps.Marker({
+  position: myLatlng,
+  map: map,
+  title: 'Hello World!'
+  });
+}
+
+function goToLocation(Lat, Lng) {
+  var myLatlng = new google.maps.LatLng(Lat, Lng);
+  var mapOptions = {
+    zoom: 13,
+    center: myLatlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  var marker = new google.maps.Marker({
+  position: myLatlng,
+  map: map,
+  title: 'CENTER POINTER'
+  });
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAsykPdvWSTP8uKVQCv27poiNwimwVQ7ds&sensor=true&callback=initialize";
+  document.body.appendChild(script);
+}
+
+
+
+window.onload = loadScript;
+
+
+
+
+
+
